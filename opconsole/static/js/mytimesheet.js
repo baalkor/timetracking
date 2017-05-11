d12bee8b9a35ee521639de316d68f5c98427afb5
baalkor <baalkor@gmail.com>
2017-05-10 20:32:15 -0600

    x<yx<

--- opconsole/static/js/mytimesheet.js
+++ opconsole/static/js/mytimesheet.js
@@ -1,13 +1,28 @@
+function setURL(parm, val) {
+      url = location.href;
+      if ( url.indexOf("?") === -1 ) {
+            location.href = url + "?" + parm + '='+ val;
+        } else {
+            if ( url.indexOf(param) === -1 ) {
+                location.href += "&" + parm + '='+ val;
+            } else {
+
+                posParm = url.indexOf(param) + parm.length + 1;
+
+            }
+        }
+}
+
+
+function showDateAt() {
+    setURL( "date", $("#datechooser").val());
+}
+
 $(document).ready(function(){
     $('#datechooser').datepicker({
       "dateFormat":"yy-mm-dd",
       showButtonPanel: false
     }).on("change", function(){
-        location.href = "/mytimesheet/?date=" + $(this).val();
+        setURL("date", $(this).val());
     });
 } );
-
-
-function showDateAt() {
-    location.href = "/mytimesheet/?date=" + $("#datechooser").val();
-}
