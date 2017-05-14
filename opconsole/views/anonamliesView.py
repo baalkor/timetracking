f4c50053d65506c4ab823150708e30dfc6e6d565
baalkor <baalkor@gmail.com>
2017-05-13 20:14:10 -0600

    dfsfdsfds

--- opconsole/views/anonamliesView.py
+++ opconsole/views/anonamliesView.py
@@ -1,35 +1,45 @@
 from django.views.generic import ListView
-from django.shortcuts import render
-from django.template import Context
 from opconsole.models import Timesheets, Device, Employes
-from django.db.models import Count
-from itertools import groupby
+from django.db.models import Count, F, Q
 from django.db.models.functions import Extract
+from django.conf import settings
+from django.shortcuts import get_object_or_404
 
 class AnomaliesView(ListView):
     template_name = "opconsole_anomalies.html"
     model = Timesheets
     context_object_name = 'anomalies'
 
-    _dbg_query = ""
+
 
     def get_context_data(self, **kwargs):
         context = super(AnomaliesView , self).get_context_data(**kwargs)
-        context["query"] = self._dbg_query
         return context
 
     def get_queryset(self):
+        isContentAdmin = self.request.user.groups.filter(name=settings.ADMIN_GROUP).exists()
+        currUser = get_object_or_404(Employes, user__id=self.request.user.id)
+
+        if isContentAdmin:
+            addFilter = Q()
+        else:
+            addFilter = Q(user=currUser)
+
+
         querySet = Timesheets.objects.values(
             "user__user__last_name",
             "user__user__first_name",
-            "user__user__id"
+            "user__id"
         ).annotate(
             year=Extract("recptTime", "year"),
             month=Extract("recptTime", "month"),
             day=Extract("recptTime", "day")
-        ).annotate(
+        ).filter(status='0').annotate(
             nbTimb=Count("day")
-        ).all()
-        self._dbg_query = querySet.query
+        ).annotate(
+            odd=F("nbTimb") % 2
+        ).filter(odd=True).filter(addFilter)
+
+
         return querySet
 
